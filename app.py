50a4235ca376506ba8b63d321ac4f8af490c4d9e
cersei <ka_du5@yahoo.com>
2025-09-12 11:18:27 +0200

    fix: from langfuse imory observe

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from langfuse import Langfuse
 from langfuse.openai import OpenAI as LangfuseOpenAI
-from langfuse.decorators import observe
+from langfuse import observe
 
 
 load_dotenv()
